bca979ac057bfb88242e5d3de5c34d451de2891a
mrkai1253 <mrkai1253@gmail.com>
2018-10-10 08:06:38 +0530

    Typo

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, list_users, message_user
 
 urlpatterns = [
-    path('', home),
+    path('', home, name = 'home'),
     path('users/', list_users, name='users'),
     path('users/<username>', message_user, name='message-user'),
 ]
