cd27040586018fc3baa1a63a1a8d8dabc481b3e7
mrkai1253 <mrkai1253@gmail.com>
2018-10-10 19:55:31 +0530

    Screenshot

--- djangochatapp/settings.py
+++ djangochatapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'z(kj2!%*$*w^%a+@h)1!_c(bt7%4@1!ifx*fyahxhfomf6c&_i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.42.55','192.168.43.3']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
