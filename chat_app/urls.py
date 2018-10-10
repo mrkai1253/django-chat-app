1cad31ffe8abe88b295a2dad40bcfb30e08b6160
mrkai1253 <mrkai1253@gmail.com>
2018-10-10 22:24:25 +0530

    Added login form

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, list_users, message_user
+from .views import home, list_users, message_user,login_func
 
 urlpatterns = [
     path('', home, name = 'home'),
+    path('login/',login_func,name = 'login'),
     path('users/', list_users, name='users'),
     path('users/<username>', message_user, name='message-user'),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,36 +1,51 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from .forms import MessageForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from .models import MessageModel
 
 
-def home(request):
-    sender = authenticate(username='Shaji', password='shajithomas1253')
-    if sender is not None:
-        login(request, sender)
-        print("{} authenticated".format(request.user))
-        users = User.objects.all().filter(is_staff=False)
 
-        form = MessageForm(request.POST or None)
-        if form.is_valid():
-            print()
 
-            msgmodel = MessageModel(receiver=User.objects.get(username=request.POST['dropdown']),
-                                    sender_name=request.user,
-                                    message=form.cleaned_data['message'])
-            msgmodel.save()
+def login_func(request):
 
-            form = MessageForm()
-
+    if request.method == 'POST':
+        user = authenticate(username = request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            login(request,user)
+            print("{} authenticated".format(request.user))
+            return redirect('home')
         else:
-            print("Error")
+            print('Wrong credentials')
 
-        messages = reversed(MessageModel.objects.all().filter(sender_name=request.user).order_by('-id')[:4])
+    return render(request,'login.html')
 
-    return render(request, 'index.html', {'messageform': form, 'users': users, 'messages': messages})
 
+def home(request):
+        if request.user.is_authenticated:
+            print("{} authenticated".format(request.user))
+            users = User.objects.all().filter(is_staff=False).exclude(username = request.user)
+
+            form = MessageForm(request.POST or None)
+            if form.is_valid():
+                print()
+
+                msgmodel = MessageModel(receiver=User.objects.get(username=request.POST['dropdown']),
+                                        sender_name=request.user,
+                                        message=form.cleaned_data['message'])
+                msgmodel.save()
+
+                form = MessageForm()
+
+            else:
+                print("Error")
+
+            messages = reversed(MessageModel.objects.all().filter(sender_name=request.user).order_by('-id')[:4])
+
+            return render(request, 'index.html', {'messageform': form, 'users': users, 'messages': messages})
+        else:
+            return redirect('login')
 
 def list_users(request):
     users = User.objects.all().filter(is_staff=False)
@@ -40,4 +55,4 @@ def list_users(request):
 def message_user(request,username):
     print(username)
     return HttpResponse('{} Selected'.format(username))
-    pass
+    
